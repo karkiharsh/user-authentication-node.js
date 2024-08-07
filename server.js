8e1a903e0b77f55d92172ae6f947278ded9d676c
Harsh Karki <harsh.karki@worksimpli.io>
2024-08-07 18:59:02 +0100

    test get request and create post request

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const app = express();
+//setting up routes
 
+app.use(express.json());
+const users = [{ name: "Name" }];
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/users", (req, res) => {
+  const user = { name: req.body.name, password: req.body.password };
+  users.push(user);
+  res.status(201).send(); //send a blank response
+});
 app.listen(3000);
