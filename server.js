95766c6a8bca02fde0b9f7756b79de036ca8f1a9
Harsh Karki <harsh.karki@worksimpli.io>
2024-08-08 18:22:55 +0100

    add bcrypt salt and hash the passwords

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+
 //setting up routes
 
 app.use(express.json());
-const users = [{ name: "Name" }];
+const users = [];
 app.get("/users", (req, res) => {
   res.json(users);
 });
 
-app.post("/users", (req, res) => {
-  const user = { name: req.body.name, password: req.body.password };
-  users.push(user);
-  res.status(201).send(); //send a blank response
+app.post("/users", async (req, res) => {
+  //create a salt
+
+  try {
+    const salt = await bcrypt.genSalt(); //salt is different to keep the hashes of same passwords different
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    console.log(salt);
+    console.log(hashedPassword);
+
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send(); //send a blank response
+  } catch {
+    res.status(500).send();
+  }
 });
 app.listen(3000);
